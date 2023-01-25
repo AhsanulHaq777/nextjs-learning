88e94ba51f8f191deab58d83ce8b0ca49a699d40
ahsanulhaq <ulhaq.ahsan777@gmail.com>
2023-01-25 23:21:11 +0500

    just created a new nextjs 13 proj and integrated with chakra ui

---
+++ nexjs-pricing-ui/src/app/layout.tsx
@@ -0,0 +1,13 @@
+import ChakraWrapper from "@/components/chakraWrapper"
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <head />
+      <body><ChakraWrapper>{children}</ChakraWrapper></body>
+    </html>
+  )
+}
